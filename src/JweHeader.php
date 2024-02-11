5b51475130c13cd80834099a4c1cbc7da538ac10
Daniel McCarthy <dmacl0l1992@gmail.com>
2024-02-11 14:42:07 +1100

    add doc blocks and return typehints to the JweHeader class

--- src/JweHeader.php
+++ src/JweHeader.php
@@ -2,32 +2,51 @@
 
 namespace SecureTokenPhp;
 
-use SecureTokenPhp\Exceptions\InvalidHeaderException;
-
+/**
+ * Represents the header for a JSON Web Encryption (JWE).
+ */
 class JweHeader extends Header
 {
     private const ALGORITHM = 'alg';
-    private const TYPE = 'typ';
 
-
-    public function setAlgorithm(JweAlgorithmEnum $algo)
+    /**
+     * Sets the key management algorithm used for the JWE.
+     *
+     * @param JweAlgorithmEnum $algo The algorithm enum value.
+     * @return void
+     */
+    public function setAlgorithm(JweAlgorithmEnum $algo): void
     {
         $this->headers[self::ALGORITHM] = $algo->value;
     }
 
-
+    /**
+     * Retrieves the key management algorithm set for the JWE, if any.
+     *
+     * @return JweAlgorithmEnum|null The key management algorithm or null if not set.
+     */
     public function getAlgorithm(): ?JweAlgorithmEnum
     {
-        $algValue = $this->headers['alg'] ?? null;
+        $algValue = $this->headers[self::ALGORITHM] ?? null;
         return $algValue !== null ? JweAlgorithmEnum::tryFrom($algValue) : null;
     }
 
-    public function setEncAlgorithm(JweContentEncryptionEnum $algo)
+    /**
+     * Sets the content encryption algorithm used for the JWE.
+     *
+     * @param JweContentEncryptionEnum $algo The content encryption algorithm enum value.
+     * @return void
+     */
+    public function setEncAlgorithm(JweContentEncryptionEnum $algo): void
     {
         $this->headers['enc'] = $algo->value;
     }
 
-
+    /**
+     * Retrieves the content encryption algorithm set for the JWE, if any.
+     *
+     * @return JweContentEncryptionEnum|null The content encryption algorithm or null if not set.
+     */
     public function getEncAlgorithm(): ?JweContentEncryptionEnum
     {
         $algValue = $this->headers['enc'] ?? null;
