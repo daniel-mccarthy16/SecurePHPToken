d86ca60021926de52d357fca67d5323ff61027e0
Daniel McCarthy <dmacl0l1992@gmail.com>
2024-02-11 14:39:20 +1100

    add doc blocks and specific return types for JwsHeader subclass

--- src/JwsHeader.php
+++ src/JwsHeader.php
@@ -2,19 +2,32 @@
 
 namespace SecureTokenPhp;
 
+/**
+ * Represents the header for a JSON Web Signature (JWS).
+ */
 class JwsHeader extends Header
 {
     private const ALGORITHM = 'alg';
 
-    public function setAlgorithm(JwsAlgorithmEnum $algo)
+    /**
+     * Sets the algorithm used for signing the JWS.
+     *
+     * @param JwsAlgorithmEnum $algo The signing algorithm.
+     * @return void
+     */
+    public function setAlgorithm(JwsAlgorithmEnum $algo): void
     {
         $this->headers[self::ALGORITHM] = $algo->value;
     }
 
-
+    /**
+     * Retrieves the algorithm used for signing the JWS, if set.
+     *
+     * @return JwsAlgorithmEnum|null The signing algorithm, or null if not set.
+     */
     public function getAlgorithm(): ?JwsAlgorithmEnum
     {
-        $algValue = $this->headers['alg'] ?? null;
+        $algValue = $this->headers[self::ALGORITHM] ?? null;
         return $algValue !== null ? JwsAlgorithmEnum::tryFrom($algValue) : null;
     }
 }
